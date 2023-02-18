21cdd59edddbef0cb5a6d254ce4969eb7f526c18
Byunghpyo Ko <kbp4154@naver.com>
2023-02-18 21:36:58 +0900

    Time: 81 ms (91.99%), Space: 47.6 MB (56.26%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -2,22 +2,14 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {
-    const arr = s.split("");
-    let answer = 0
-    let checkArr = [];
-    
-    for(let i = 0; i < arr.length; i++){
-        if(checkArr.includes(arr[i])){
-            if(answer < checkArr.length) answer = checkArr.length;
-            const checkNum = checkArr.indexOf(arr[i])
-            const tempArr = checkArr.slice(checkNum + 1, checkArr.length)
-            checkArr = tempArr
-            checkArr.push(arr[i])
-        } else checkArr.push(arr[i])
+const lengthOfLongestSubstring = function(s) {
+    let subStr = "";
+    let maxLength = 0;
+  
+    for (let i = 0; i < s.length; i++) {
+        if (subStr.includes(s[i])) subStr = subStr.slice(subStr.indexOf(s[i]) + 1);
+        subStr += s[i];
+        if (maxLength < subStr.length) maxLength = subStr.length;
     }
-    
-    if(answer < checkArr.length) answer = checkArr.length;
-    if (!answer) return s.length
-    return answer
+    return maxLength;
 };
