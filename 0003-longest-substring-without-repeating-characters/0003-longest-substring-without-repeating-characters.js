34dc493aa591f5b92f17d6aeb2a504962a18d4ad
Byunghpyo Ko <kbp4154@naver.com>
2023-02-18 21:31:43 +0900

    Time: 99 ms (61.56%), Space: 48.7 MB (30.17%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    const arr = s.split("");
+    let answer = 0
+    let checkArr = [];
+    
+    for(let i = 0; i < arr.length; i++){
+        if(checkArr.includes(arr[i])){
+            if(answer < checkArr.length) answer = checkArr.length;
+            const checkNum = checkArr.indexOf(arr[i])
+            const tempArr = checkArr.slice(checkNum + 1, checkArr.length)
+            checkArr = tempArr
+            checkArr.push(arr[i])
+        } else {
+            checkArr.push(arr[i])
+        }
+    }
+    
+    if(answer < checkArr.length) answer = checkArr.length;
+
+    if (!answer) return s.length
+    return answer
+};
