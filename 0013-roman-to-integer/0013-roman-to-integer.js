5703adb9ee3dfbd6697b8889f789a20c483a3bdb
Byunghpyo Ko <kbp4154@naver.com>
2023-02-26 19:04:29 +0900

    Time: 124 ms (78.79%), Space: 47.1 MB (48.33%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.js
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -3,25 +3,30 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    let answer = 0
-    let numArr = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
-    let symbolArr = ['M', 'CM', 'D', 'CD', 'C','XC', 'L','XL', 'X','IX', 'V', 'IV', 'I']
-    
-    
-    
-    for(let i = 0; i < s.length; i++){
-        if(i + 1 < s.length){
-            const temp = s[i] + s[i + 1]
-            if(symbolArr.indexOf(temp) !== -1){
-                answer += numArr[symbolArr.indexOf(temp)]
-                i++
-                continue
-            }
-        }
-        const temp = s[i]
-        if(symbolArr.indexOf(temp) !== -1)answer += numArr[symbolArr.indexOf(temp)]
+      const sym = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
     }
 
-    return answer
+    let result = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        const cur = sym[s[i]];
+        const next = sym[s[i + 1]];
+
+        if (cur < next) {
+            result += next - cur;
+            i++;
+        } else {
+            result += cur;
+        }
+    }
+
+    return result;
 };
 
