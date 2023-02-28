3a47ee6557edefe117842df98ed7f8e721ff233f
Byunghpyo Ko <kbp4154@naver.com>
2023-02-28 22:38:48 +0900

    Update 0014-longest-common-prefix.js

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -7,9 +7,7 @@ var longestCommonPrefix = function(strs) {
     
     for(let i = 0; i < strs[0].length; i++) {
         for(let s of strs) {
-            if(s[i] !== strs[0][i]) {
-                return s.slice(0, i);
-            }
+            if(s[i] !== strs[0][i]) return s.slice(0, i);
         }
     }   
     return strs[0];
