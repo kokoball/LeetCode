f77e10d9368c9e21901adade8c8c4395e348ea3e
Byunghpyo Ko <kbp4154@naver.com>
2023-03-05 23:23:25 +0900

    Time: 54 ms (93.22%), Space: 41.4 MB (94.88%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
+    const numArr = ['','','abc','def','ghi','jkl','mno','pqrs','tuv','wxyz']
+    let answer = []
+    
+    for(let i = 0; i < digits.length; i++ ){
+        const nowNum = +digits[digits.length - 1 - i]
+        
+        if (answer.length === 0 ) answer = numArr[nowNum].split("")
+        else {
+            const temp = []
+            for(let j = 0; j < numArr[nowNum].length; j++){
+                for(let k = 0; k < answer.length; k++){
+                    temp.push(numArr[nowNum][j] + answer[k])
+                }
+            }
+            answer = temp
+        }
+    }
+    
+    return answer
+};
