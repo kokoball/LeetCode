bf63c5c9458098fa51e19ac4329a7890bf8ca7a1
Byunghpyo Ko <kbp4154@naver.com>
2023-03-14 23:12:50 +0900

    Time: 138 ms (28.78%), Space: 44.7 MB (19.85%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.js
+++ 0045-jump-game-ii/0045-jump-game-ii.js
@@ -3,16 +3,24 @@
  * @return {number}
  */
 var jump = function(nums) {
-    const dp = [0];
-    for(let i = 1; i < nums.length; i++){
-        dp[i] = Number.MAX_VALUE;
+    const length = nums.length;
+    const dp = Array(length).fill(Infinity);
+    
+    if(length === 1)return 0;
+    
+    dp[0] = 0;
+    
+    for(let i = 0; i < length; i++){
+        const distance = nums[i];
+        const next = dp[i] + 1;
+                
+        for(let j = 1; j <= distance; j++){
+            if(i + j >= length){
+                break;
+            }
+            dp[i + j] = Math.min(next, dp[i + j]);
+        }
     }
     
-    nums.map((num, i) => {
-        for(let j = 1; j <= num && i+j < nums.length; j++){
-            dp[i+j] = dp[i] + 1 < dp[i+j] ? dp[i] + 1 : dp[i+j];
-        }
-    })
-    
-    return dp[nums.length - 1];
+    return dp[length - 1];
 };
