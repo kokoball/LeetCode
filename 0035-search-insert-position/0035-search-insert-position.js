bdf7df6e263fa58779dcedf1cd7d695e1347a8ac
Byunghpyo Ko <kbp4154@naver.com>
2023-03-15 21:51:15 +0900

    Time: 52 ms (95.09%), Space: 41.8 MB (73.97%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -4,11 +4,14 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-    let left = 0, right = nums.length - 1;
-    while (left <= right) {
-        if (left === right) return target <= nums[left] ? left : left +1;
-        const mid = left + Math.floor((right - left)/2);
-        if (nums[mid] < target) left = mid + 1;
-         else right = mid;
+    // if(nums.indexOf(target) !== -1) return nums.indexOf(target)
+    let answer = 0
+    
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] >= target) break;
+        answer++
     }
+    
+    return answer
+    
 };
