c38b0fd34536a8d1121bbacd538bb21af7dff6e0
Byunghpyo Ko <kbp4154@naver.com>
2023-03-19 15:23:45 +0900

    Time: 66 ms (93.02%), Space: 44 MB (24.19%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.js
+++ 0069-sqrtx/0069-sqrtx.js
@@ -2,11 +2,20 @@
  * @param {number} x
  * @return {number}
  */
-var mySqrt = (x) => {
-  let r = x
-  while (r * r > x) {
-    r = Math.floor((r + x / r) / 2)
+var mySqrt = function (x) {
+  let left = 1;
+  let right = x;
+  let ans = null;
+  while (left <= right) {
+    let mid = parseInt((left + right) / 2);
+    
+    if (mid * mid === x) return mid;
+    if (mid * mid > x) {
+      right = mid - 1;
+    } else {
+      ans = mid;
+      left = mid + 1;
+    }
   }
-
-  return r
-}
+  return ans;
+};
