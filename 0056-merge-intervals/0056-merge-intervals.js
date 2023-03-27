50b8301254aaea611cf26ce875500a0e885abbd3
Byunghpyo Ko <kbp4154@naver.com>
2023-03-27 23:59:25 +0900

    Time: 90 ms (96.85%), Space: 49.9 MB (29.91%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+const merge = (intervals) => {
+    intervals.sort((a, b) => {
+        return a[0] - b[0]
+    })
+
+    const answer = [[...intervals[0]]];
+    for (let i=1; i < intervals.length; i++) {
+        const lastIdx = answer.length-1;
+        
+        if (answer[lastIdx][1] >= intervals[i][0]) {
+            if (answer[lastIdx][1] < intervals[i][1]) {
+                answer[lastIdx][1] = intervals[i][1]
+            }
+        } else {
+            answer.push([...intervals[i]])
+        }
+    }
+    return answer;
+};
