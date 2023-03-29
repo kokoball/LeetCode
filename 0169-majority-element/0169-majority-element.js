1110fd2a0d189ed6ae7af6f15f64ef014b37cee5
Byunghpyo Ko <kbp4154@naver.com>
2023-03-29 23:51:51 +0900

    Time: 58 ms (94.60%), Space: 46.2 MB (8.09%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -3,26 +3,6 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    if(nums.length === 1) return nums[0];
-    
-    let answer = 0;
-    let temp = 0;
-    let map = new Map();
-    
-    for(let x of nums) {
-        if(!map.get(x)) {
-            map.set(x, 1);
-        } else {
-            map.set(x, map.get(x) + 1);
-        }
-    }
-    
-    for(let [key, val] of map) {
-        if(temp < val) {
-            temp = val;
-            answer = key;
-        }
-    }
-    
-    return answer;
-};
+    nums.sort((a, b) => a - b);
+    return nums[Math.floor(nums.length / 2)];
+}; 
