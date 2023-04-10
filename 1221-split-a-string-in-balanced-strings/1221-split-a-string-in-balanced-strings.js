7160f9945c8cd94799cae299af46feb3fdbe8bf1
Byunghpyo Ko <kbp4154@naver.com>
2023-04-10 11:02:30 +0900

    Time: 52 ms (91.16%), Space: 42.1 MB (55.23%) - LeetHub

--- 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.js
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.js
@@ -6,14 +6,9 @@ var balancedStringSplit = function(s) {
     let result = 0
     let balance = 0
     for (let i = 0; i < s.length; i++) {
-        if (s[i] === 'R') {
-            balance++
-        } else if (s[i] ==='L') {
-            balance--
-        }
-        if (balance === 0) {
-            result++
-        }
+        if (s[i] === 'R') balance++
+        if (s[i] ==='L') balance--
+        if (balance === 0) result++
     }
     return result
 };
