70a9578e47f35fde47cbc10821caf0fb262b4ea0
Byunghpyo Ko <kbp4154@naver.com>
2023-04-14 20:51:43 +0900

    Time: 64 ms (72.10%), Space: 43.9 MB (75.02%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} path
+ * @return {string}
+ */
+var simplifyPath = function(path) {
+    let stack = [];
+
+    paths = path.split('/').filter(path=>path !== '')
+
+    for (const element of paths) {
+        if (element === '..') {
+            stack.pop()
+        } else if (element === '.') {
+            continue;
+        } else {
+            stack.push(element)
+        }
+    }
+    
+    return '/'+stack.join('/')
+};
